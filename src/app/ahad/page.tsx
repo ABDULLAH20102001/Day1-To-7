fae6db8b020eabcc1612fbc3d183a0f2814b8fbf
ABDULLAH SALEEM <163736520+ABDULLAH20102001@users.noreply.github.com>
2025-02-08 00:06:05 +0500

    1
    1

---
+++ src/app/ahad/page.tsx
@@ -0,0 +1,19 @@
+// 'use client';
+// import React from 'react';
+// import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+// import Signup from '../Signup/page';
+// import Login from '../Signup/Login/page';
+
+
+// const Pa1 = () => {
+//   return (
+//     <Router>  {/* Wrap your Routes inside BrowserRouter */}
+//       <Routes>
+//         <Route path="/signup" element={<Signup />} />
+//         <Route path="/login" element={<Login />} />
+//       </Routes>
+//     </Router>
+//   );
+// };
+
+// export default Pa1;
